c076fa825802a4e0d81eb151504f52c002a8dd30
Derek Black <black@ksu.edu>
2017-05-01 13:06:41 -0500

    Update circularBuffer.h

--- circularBuffer.h
+++ circularBuffer.h
@@ -1,19 +1,27 @@
+/*
+    * Author: Derek Black
+    * Description: Circular buffer for strings
+    * Date: 5/1/17
+*/
+
 #ifndef CIRCULAR_BUFFER_H
 #define CIRCULAR_BUFFER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Includes */
-#include "USART.h"
 #include <string.h>
-/* -------- */
 
 /* Global Variables */
 #define MAXCOMMBUFFER 2048
 #define MAX_STRLEN 2048
-/* ---------------- */
 
 typedef struct _commBuffer_t {
 	uint32_t head;
 	uint32_t tail;
+	uint32_t size;
 	char buffer[MAXCOMMBUFFER + 1];
 } commBuffer_t;
 
@@ -27,7 +35,7 @@ void putChar(commBuffer_t* comm, char ch);
 //Get character from buffer and update tail;
 char getChar(commBuffer_t* comm);
 //put C string into buffer
-void putStr(commBuffer_t* comm, char* str, uint8_t length);
+void putStr(commBuffer_t* comm, char* str);
 //get C string from buffer
 void getStr(commBuffer_t* comm, char* str);
 // Check if buffer has exceeded max size
@@ -35,5 +43,8 @@ void checkBufferOverflowStatus(commBuffer_t* comm);
 // Get buffer's current size
 uint32_t bufferSize(commBuffer_t* comm);
 
+#ifdef __cplusplus
+}
 #endif
 
+#endif /* CIRCULAR_BUFFER_H */
